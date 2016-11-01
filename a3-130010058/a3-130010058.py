70adf5ea88a6d2f4deb6410aadaead00505cc0cb
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2016-11-01 10:17:39 +0000

    First change to assignment on November 1st

--- a3-130010058/a3-130010058.py
+++ a3-130010058/a3-130010058.py
@@ -21,23 +21,28 @@ def q1():
     Y_Euler = [x]
     U_Euler = [u]
     V_Euler = [v]
+    E_Euler = [0.5*m*(u**2 + v**2)]
     X_Euler_Symplectic = [x]
     Y_Euler_Symplectic = [x]
     U_Euler_Symplectic = [u]
     V_Euler_Symplectic = [v]
+    E_Euler_Symplectic = [0.5*m*(u**2 + v**2)]
     X_RK2 = [x]
     Y_RK2 = [x]
     U_RK2 = [u]
     V_RK2 = [v]
+    E_RK2 = [0.5*m*(u**2 + v**2)]
     X_Boris = [x]
     Y_Boris = [x]
     U_Boris = [u]
     V_Boris = [v]
+    E_Boris = [0.5*m*(u**2 + v**2)]
     while t < tf:
         U_Euler.append(U_Euler[-1] + V_Euler[-1]*B)
         V_Euler.append(V_Euler[-1] - U_Euler[-1]*B)
         X_Euler.append(X_Euler[-1] + U_Euler[-2]*dt)
         Y_Euler.append(Y_Euler[-1] + V_Euler[-2]*dt)
+        E_Euler.append(0.5*m*(U_Euler[-1]**2 + V_Euler[-1]**2))
         # Euler Scheme ends here
         U_Euler_Symplectic.append(U_Euler_Symplectic[-1] +
                                   V_Euler_Symplectic[-1]*B)
@@ -47,6 +52,8 @@ def q1():
                                   U_Euler_Symplectic[-1]*dt)
         Y_Euler_Symplectic.append(Y_Euler_Symplectic[-1] +
                                   V_Euler_Symplectic[-1]*dt)
+        E_Euler_Symplectic.append(0.5*m*(U_Euler_Symplectic[-1]**2 +
+                                         V_Euler_Symplectic[-1]**2))
         # Euler Symplectic scheme ends here
         ku1 = V_RK2[-1]*B*dt
         kv1 = -U_RK2[-1]*B*dt
@@ -60,6 +67,18 @@ def q1():
         V_RK2.append(V_RK2[-1] + kv2)
         X_RK2.append(X_RK2[-1] + kx2)
         Y_RK2.append(Y_RK2[-1] + ky2)
+        E_RK2.append(0.5*m*(U_RK2[-1]**2 + V_RK2[-1]**2))
         # RK2 scheme ends here
+        uminus = U_Boris[-1]
+        vminus = V_Boris[-1]
+        alpha = q*B*dt/(2.0*m)
+        uplus = ((1-alpha*alpha)*uminus + 2*alpha*vminus)/(alpha*alpha + 1.0)
+        vplus = (vminus*(1 - alpha*alpha) - 2*alpha*uminus)/(1 + alpha*alpha)
+        U_Boris.append(uplus)
+        V_Boris.append(vplus)
+        X_Boris.append(X_Boris[-1] + U_Boris[-1]*dt)
+        Y_Boris.append(Y_Boris[-1] + V_Boris[-1]*dt)
+        E_Boris.append(0.5*m*(U_Boris[-1]**2 + V_Boris[-1]**2))
+        # Boris scheme ends here
         t = t + dt
     pass
