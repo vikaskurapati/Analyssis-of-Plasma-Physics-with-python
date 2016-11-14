a0e65553e3b1391a8ee2d2ce26ec4dc924dc29ae
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2016-11-14 01:11:53 +0000

    Some rubbish output

--- a3-130010058/a3-130010058.py
+++ a3-130010058/a3-130010058.py
@@ -1,84 +1,220 @@
 import numpy as np
+# import pymetabiosis.auto
 from matplotlib import pyplot as plt
+import copy
 
 
-def q1():
-    '''taking a unit mass with unit charge with initial velocity of 0i+1j
-    under the influence of 1magnetic field starting at (1,0)'''
-    m = 1.0
-    B = 1.0
-    q = 1.0
-    T = 2*np.pi  # Time period for the following specifications is 2pi seconds.
-    tf = 10*t
-    dt = 0.05*T
+def q1(Dt, m=1.0, q=1.0, B=1.0, xi=-1.0 + 0.0j, ui=0.0 + 1.0j, plot=True):  # Dt is the dt scaling factor, dt = Dt*T
+    T = 2.0*np.pi*m/(q*B)
+    dt = Dt*T
+    tf = 10*T
+    x = [xi]  # initial position of the particle
+    vi = [ui]  # initial velocity of the particle
+    x_euler = copy.copy(x)
+    v_euler = copy.copy(vi)
+    x_euler_simplicit = copy.copy(x)  # simplicit means semi implicit
+    v_euler_simplicit = copy.copy(vi)
+    x_RK2 = copy.copy(x)
+    v_RK2 = copy.copy(vi)
+    x_Boris = copy.copy(x)
+    v_Boris = copy.copy(vi)
+    T = [0.0]
+    # Denoting a vector with a complex number where real part is i value and
+    # imaginary part is j value
     t = 0.0
-    u = 0.0
-    v = 1.0
-    x = 1.0
-    y = 0.0
-    T = [t]
-    X_Euler = [x]
-    Y_Euler = [x]
-    U_Euler = [u]
-    V_Euler = [v]
-    E_Euler = [0.5*m*(u**2 + v**2)]
-    X_Euler_Symplectic = [x]
-    Y_Euler_Symplectic = [x]
-    U_Euler_Symplectic = [u]
-    V_Euler_Symplectic = [v]
-    E_Euler_Symplectic = [0.5*m*(u**2 + v**2)]
-    X_RK2 = [x]
-    Y_RK2 = [x]
-    U_RK2 = [u]
-    V_RK2 = [v]
-    E_RK2 = [0.5*m*(u**2 + v**2)]
-    X_Boris = [x]
-    Y_Boris = [x]
-    U_Boris = [u]
-    V_Boris = [v]
-    E_Boris = [0.5*m*(u**2 + v**2)]
     while t < tf:
-        U_Euler.append(U_Euler[-1] + V_Euler[-1]*B)
-        V_Euler.append(V_Euler[-1] - U_Euler[-1]*B)
-        X_Euler.append(X_Euler[-1] + U_Euler[-2]*dt)
-        Y_Euler.append(Y_Euler[-1] + V_Euler[-2]*dt)
-        E_Euler.append(0.5*m*(U_Euler[-1]**2 + V_Euler[-1]**2))
-        # Euler Scheme ends here
-        U_Euler_Symplectic.append(U_Euler_Symplectic[-1] +
-                                  V_Euler_Symplectic[-1]*B)
-        V_Euler_Symplectic.append(V_Euler_Symplectic[-1] -
-                                  U_Euler_Symplectic[-1]*B)
-        X_Euler_Symplectic.append(X_Euler_Symplectic[-1] +
-                                  U_Euler_Symplectic[-1]*dt)
-        Y_Euler_Symplectic.append(Y_Euler_Symplectic[-1] +
-                                  V_Euler_Symplectic[-1]*dt)
-        E_Euler_Symplectic.append(0.5*m*(U_Euler_Symplectic[-1]**2 +
-                                         V_Euler_Symplectic[-1]**2))
-        # Euler Symplectic scheme ends here
-        ku1 = V_RK2[-1]*B*dt
-        kv1 = -U_RK2[-1]*B*dt
-        kx1 = U_RK2[-1]*dt
-        ky1 = V_RK2[-1]*dt
-        ku2 = (V_RK2[-1] + ku1/2.)*B*dt
-        kv2 = -(U_RK2[-1] + kv1/2.)*B*dt
-        kx2 = dt*(ku1 + U_RK2[-1])
-        ky2 = dt*(kv1 + V_RK2[-1])
-        U_RK2.append(U_RK2[-1] + ku2)
-        V_RK2.append(V_RK2[-1] + kv2)
-        X_RK2.append(X_RK2[-1] + kx2)
-        Y_RK2.append(Y_RK2[-1] + ky2)
-        E_RK2.append(0.5*m*(U_RK2[-1]**2 + V_RK2[-1]**2))
-        # RK2 scheme ends here
-        uminus = U_Boris[-1]
-        vminus = V_Boris[-1]
-        alpha = q*B*dt/(2.0*m)
-        uplus = ((1-alpha*alpha)*uminus + 2*alpha*vminus)/(alpha*alpha + 1.0)
-        vplus = (vminus*(1 - alpha*alpha) - 2*alpha*uminus)/(1 + alpha*alpha)
-        U_Boris.append(uplus)
-        V_Boris.append(vplus)
-        X_Boris.append(X_Boris[-1] + U_Boris[-1]*dt)
-        Y_Boris.append(Y_Boris[-1] + V_Boris[-1]*dt)
-        E_Boris.append(0.5*m*(U_Boris[-1]**2 + V_Boris[-1]**2))
-        # Boris scheme ends here
+        v_euler += [v_euler[-1]+(q*B*(v_euler[-1].imag - v_euler[-1].real*1.0j)*dt)/m]
+        x_euler += [x_euler[-1] + v_euler[-2]*dt]
+        v_euler_simplicit += [v_euler_simplicit[-1]+(q*B*(v_euler_simplicit[-1].imag - v_euler_simplicit[-1].real*1.0j)*dt)/m]
+        x_euler_simplicit += [x_euler_simplicit[-1] + v_euler_simplicit[-1]*dt]
+        kv1 = q*B*(v_RK2[-1].imag - v_RK2[-1].real*1.0j)*dt/m
+        kx1 = v_RK2[-1]*dt
+        v = v_RK2[-1] + kv1/2.0
+        kv2 = dt*q*B*(v.imag - v.real*1.0j)/m
+        kx2 = dt*(kv1 + v_RK2[-1])
+        v_RK2 += [v_RK2[-1] + kv2]
+        x_RK2 += [x_RK2[-1] + kx2]
+        vminus = v_Boris[-1]
+        alpha = 0.5*q*B*dt/m
+        uplus = (1.0-alpha*alpha)*vminus.real/(1.0+alpha*alpha)+2.0*alpha*vminus.imag/(1.0+alpha*alpha)
+        vplus = -2.0*alpha*vminus.real/(1.0+alpha*alpha)+(1.0-alpha*alpha)*vminus.imag/(1.0+alpha*alpha)
+        Vplus = uplus + vplus*1.0j
+        v_Boris += [Vplus]
+        x_Boris += [x_Boris[-1] + v_Boris[-1]*dt]
         t = t + dt
-    pass
+        T += [t]
+    v_euler = np.array(v_euler)
+    x_euler = np.array(x_euler)
+    v_euler_simplicit = np.array(v_euler_simplicit)
+    E_euler = 0.5*m*abs(v_euler)*abs(v_euler)
+    diss_euler = abs(E_euler - 0.5*m*vi[0]*vi[0])
+    x_euler_simplicit = np.array(x_euler_simplicit)
+    E_euler_simplicit = 0.5*m*abs(v_euler_simplicit)*abs(v_euler_simplicit)
+    diss_euler_simplicit = abs(E_euler_simplicit - 0.5*m*vi[0]*vi[0])
+    v_RK2 = np.array(v_RK2)
+    x_RK2 = np.array(x_RK2)
+    E_RK2 = 0.5*m*abs(v_RK2)*abs(v_RK2)
+    diss_RK2 = abs(E_RK2 - 0.5*m*vi[0]*vi[0])
+    v_Boris = np.array(v_Boris)
+    x_Boris = np.array(x_Boris)
+    E_Boris = 0.5*m*abs(v_Boris)*abs(v_Boris)
+    diss_Boris = abs(E_Boris - 0.5*m*vi[0]*vi[0])
+    if plot is True:
+        plt.plot(x_euler.real, x_euler.imag)
+        plt.title('Path of particle in Euler Method')
+        plt.savefig('path_euler.png')
+        plt.close()
+        plt.plot(x_euler_simplicit.real, x_euler_simplicit.imag)
+        plt.title('Path of particle in Euler semi implicit Method')
+        plt.savefig('path_euler_simplicit.png')
+        plt.close()
+        plt.plot(x_RK2.real, x_RK2.imag)
+        plt.title('Path of particle in RK2 Method')
+        plt.savefig('path_RK2.png')
+        plt.close()
+        plt.plot(x_Boris.real, x_Boris.imag)
+        plt.title('Path of particle in Boris Method')
+        plt.savefig('path_Boris.png')
+        plt.close()
+        plt.plot(T, E_euler)
+        plt.title('Energy of particle in Euler Method')
+        plt.savefig('energy_euler.png')
+        plt.close()
+        plt.plot(T, E_euler_simplicit)
+        plt.title('Energy of particle in Euler semi implicit Method')
+        plt.savefig('energy_euler_simplicit.png')
+        plt.close()
+        plt.plot(T, E_RK2)
+        plt.title('Energy of particle in RK2 Method')
+        plt.savefig('energy_RK2.png')
+        plt.close()
+        plt.plot(T, E_Boris)
+        plt.title('Energy of particle in Boris Method')
+        plt.savefig('energy_Boris.png')
+        plt.close()
+        plt.plot(T, diss_euler)
+        plt.title('Disspiation of energy of particle in Euler Method')
+        plt.savefig('diss_euler.png')
+        plt.close()
+        plt.plot(T, diss_euler_simplicit)
+        plt.title('Disspiation of energy of particle in Euler semi implicit Method')
+        plt.savefig('diss_euler_simplicit.png')
+        plt.close()
+        plt.plot(T, diss_RK2)
+        plt.title('Disspiation of energy of particle in RK2 Method')
+        plt.savefig('diss_RK2.png')
+        plt.close()
+        plt.plot(T, diss_Boris)
+        plt.title('Disspiation of energy of particle in Boris Method')
+        plt.savefig('diss_Boris.png')
+        plt.close()
+        pass
+    else:
+        # As the particles is expected to return to the initial position as tf is multiple of T
+        return abs(x_euler[-1] - x[0]),abs(x_euler_simplicit[-1] - x[0]),abs(x_RK2[-1] - x[0]),abs(x_Boris[-1] - x[0])
+
+
+def q2(DT=[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1]):
+    Err_Euler = np.zeros(len(DT))
+    Err_Euler_simplicit = np.zeros(len(DT))
+    Err_RK2 = np.zeros(len(DT))
+    Err_Boris = np.zeros(len(DT))
+    for i, Dt in enumerate(DT):
+        Err_Euler[i], Err_Euler_simplicit[i], Err_RK2[i], Err_Boris[i] = q1(Dt, plot=False)
+    plt.loglog(DT, Err_Euler)
+    plt.title('Error in Euler scheme with time step factors')
+    plt.savefig('q2_euler.png')
+    plt.close()
+    plt.loglog(DT, Err_Euler_simplicit)
+    plt.title('Error in Euler semi implicit scheme with time step factors')
+    plt.savefig('q2_euler_simplicit.png')
+    plt.close()
+    plt.loglog(DT, Err_RK2, label='RK2')
+    plt.title('Error in RK2 scheme with time step factors')
+    plt.savefig('q2_RK2.png')
+    plt.close()
+    plt.loglog(DT, Err_Boris, label='Boris')
+    plt.title('Error in Boris scheme with time step factors')
+    plt.savefig('q2_Boris.png')
+    plt.close()
+
+
+def q3(Dt, m=1.0, q=1.0, B=1.0, xi=-1.0 + 0.0j, ui=0.0 + 1.0j, E=1.0+0.0j):
+    # Give Electric field in vector form as a complex number
+    T = 2.0*np.pi*m/(q*B)
+    dt = Dt*T
+    tf = 10*T
+    x = [xi]  # initial position of the particle
+    vi = [ui]  # initial velocity of the particle
+    x_euler = copy.copy(x)
+    v_euler = copy.copy(vi)
+    x_euler_simplicit = copy.copy(x)  # simplicit means semi implicit
+    v_euler_simplicit = copy.copy(vi)
+    x_RK2 = copy.copy(x)
+    v_RK2 = copy.copy(vi)
+    x_Boris = copy.copy(x)
+    v_Boris = copy.copy(vi)
+    T = [0.0]
+    # Denoting a vector with a complex number where real part is i value and
+    # imaginary part is j value
+    t = 0.0
+    while t < tf:
+        v_euler += [v_euler[-1]+((q*B*(v_euler[-1].imag - v_euler[-1].real*1.0j)+q*E)*dt/m)]
+        x_euler += [x_euler[-1] + v_euler[-2]*dt]
+        v_euler_simplicit += [v_euler_simplicit[-1]+((q*B*(v_euler_simplicit[-1].imag - v_euler_simplicit[-1].real*1.0j)+q*E)*dt/m)]
+        x_euler_simplicit += [x_euler_simplicit[-1] + v_euler_simplicit[-1]*dt]
+        kv1 = (q*B*(v_RK2[-1].imag - v_RK2[-1].real*1.0j)+q*E)*dt/m
+        kx1 = v_RK2[-1]*dt
+        v = v_RK2[-1] + kv1/2.0
+        kv2 = dt*(q*B*(v.imag - v.real*1.0j)+q*E)/m
+        kx2 = dt*(kv1 + v_RK2[-1])
+        v_RK2 += [v_RK2[-1] + kv2]
+        x_RK2 += [x_RK2[-1] + kx2]
+        vminus = v_Boris[-1] + q*dt*E*0.5/m
+        alpha = 0.5*q*B*dt/m
+        uplus = (1.0-alpha*alpha)*vminus.real/(1.0+alpha*alpha)+2.0*alpha*vminus.imag/(1.0+alpha*alpha)
+        vplus = -2.0*alpha*vminus.real/(1.0+alpha*alpha)+(1.0-alpha*alpha)*vminus.imag/(1.0+alpha*alpha)
+        Vplus = uplus + vplus*1.0j + q*dt*E*0.5/m
+        v_Boris += [Vplus]
+        x_Boris += [x_Boris[-1] + v_Boris[-1]*dt]
+        t = t + dt
+        T += [t]
+    # v_drift =
+    v_euler = np.array(v_euler)
+    x_euler = np.array(x_euler)
+    v_euler_simplicit = np.array(v_euler_simplicit)
+    E_euler = 0.5*m*abs(v_euler)*abs(v_euler)
+    diss_euler = abs(E_euler - 0.5*m*vi[0]*vi[0])
+    x_euler_simplicit = np.array(x_euler_simplicit)
+    E_euler_simplicit = 0.5*m*abs(v_euler_simplicit)*abs(v_euler_simplicit)
+    diss_euler_simplicit = abs(E_euler_simplicit - 0.5*m*vi[0]*vi[0])
+    v_RK2 = np.array(v_RK2)
+    x_RK2 = np.array(x_RK2)
+    E_RK2 = 0.5*m*abs(v_RK2)*abs(v_RK2)
+    diss_RK2 = abs(E_RK2 - 0.5*m*vi[0]*vi[0])
+    v_Boris = np.array(v_Boris)
+    x_Boris = np.array(x_Boris)
+    E_Boris = 0.5*m*abs(v_Boris)*abs(v_Boris)
+    diss_Boris = abs(E_Boris - 0.5*m*vi[0]*vi[0])
+    plt.plot(x_euler.real, x_euler.imag)
+    plt.title('Path of particle in Euler Method with Electric Field')
+    plt.savefig('q3_path_euler.png')
+    plt.close()
+    plt.plot(x_euler_simplicit.real, x_euler_simplicit.imag)
+    plt.title('Path of particle in Euler semi implicit Method with Electric Field')
+    plt.savefig('q3_path_euler_simplicit.png')
+    plt.close()
+    plt.plot(x_RK2.real, x_RK2.imag)
+    plt.title('Path of particle in RK2 Method with Electric Field')
+    plt.savefig('q3_path_RK2.png')
+    plt.close()
+    plt.plot(x_Boris.real, x_Boris.imag)
+    plt.title('Path of particle in Boris Method with Electric Field')
+    plt.savefig('q3_path_Boris.png')
+    plt.close()
+
+
+if __name__ == '__main__':
+    q1(0.05)
+    q2(np.linspace(0.01, 0.1, 100))
+    q3(0.05)
