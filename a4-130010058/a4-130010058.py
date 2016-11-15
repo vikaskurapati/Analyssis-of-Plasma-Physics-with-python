f00db26273cbba3451ebfcc59ce5fb5980f97b45
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2016-11-15 05:18:07 +0000

    Assignment 4 in progress

--- a4-130010058/a4-130010058.py
+++ a4-130010058/a4-130010058.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import copy
 
 k = 1.38064e-23
 e = 1.60271e-19
@@ -56,3 +57,52 @@ def q1(beam_matrix):
 
 
 def q2(beam_matrix):
+    v_min = beam_matrix[0].get_beam()[1] - 5.0*beam_matrix[0].thermal_v()
+    v_max = beam_matrix[0].get_beam()[1] + 5.0*beam_matrix[0].thermal_v()
+    for beam in beam_matrix:
+        if v_min > beam.get_beam()[1] - 5.0*beam.thermal_v():
+            v_min = beam.get_beam()[1] - 5.0*beam.thermal_v()
+        if v_max < beam.get_beam()[1] + 5.0*beam.thermal_v():
+            v_max = beam.get_beam()[1] + 5.0*beam.thermal_v()
+    v_matrix = np.linspace(v_min, v_max, 1000)
+    f = np.zeros(len(v_matrix))
+    for beam in beam_matrix:
+        f = f + beam.v_dist(v_matrix)
+    plt.plot(v_matrix, f)
+    plt.ylabel('Number Density')
+    plt.xlabel('V')
+    plt.title('Initial Distribution with respect to velocity')
+    plt.savefig('q2.png')
+    plt.close()
+    return f, v_matrix
+
+
+def q3(f, v_matrix, mw):
+    dv = v_matrix[1] - v_matrix[0]
+    rho = np.sum(f*v_matrix)
+    v_mean = np.sum(v_matrix*f)*dv/rho
+    sigma = np.sqrt(np.sum((v_matrix - v_mean)**2*f*dv)/rho)
+    t = sigma*sigma*mw*m_p/# coding=utf-8
+    return rho, v_mean, t
+
+
+def q4(beam, v):
+    plt.plot(v, beam.v_dist(vel))
+    plt.xlabel('V')
+    plt.ylabel('n(V)')
+    plt.title('equilibrium distribution with respect to velocity')
+    plt.savefig('q4.png')
+    plt.close()
+
+
+def q5(initial_f, mean_f, dv, freq, m, F):
+    dt = 0.1/freq
+    f_mid = np.zeros((1, len(initial_f)))
+    f_mid[0, :] = copy.copy(f)
+    while (np.sum((f[-1] - mean_f)**2)**0.5/np.sum(mean_f) >= 0.01):
+        f = copy.copy(f_mid[-1])
+        bgk_rhs = col_freq*(fmean - f)
+        extf_term = ext_force/(mass*mp)*delf(fmean, dv)
+        f += (bgk_rhs + ext_force)*delt
+        f_evol = np.vstack([f_evol, f.copy()])
+    return f_evol
